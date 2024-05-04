7ee215bc435ed93b1ca2053f531c63260175a857
ShivamD26 <shivam2604darekar@gmail.com>
2024-05-04 10:26:36 +0530

    catalogItem

--- backend/app.js
+++ backend/app.js
@@ -54,13 +54,13 @@ app.post("/register", async (req, res) => {
     }   
 });
 
-app.get("/employeedata", async (req, res) => {
-    try{
-        const employees = await User.find({});
-        res.status(200).json(employees);
-    } 
-    catch (error) {
-        console.log(error);
-    }   
-}); 
+// app.get("/employeedata", async (req, res) => {
+//     try{
+//         const employees = await User.find({});
+//         res.status(200).json(employees);
+//     } 
+//     catch (error) {
+//         console.log(error);
+//     }   
+// }); 
 
